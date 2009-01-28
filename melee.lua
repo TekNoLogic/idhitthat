93522337377b15c35faec0491ff712c49958770c
Tekkub <tekkub@gmail.com>
2009-01-28 01:13:34 -0700

    Add tank meleee attack tables

--- melee.lua
+++ melee.lua
@@ -61,3 +61,29 @@ tek_register("Interface\\Icons\\INV_Sword_26", function(self)
 
 	tip:Show()
 end, tip)
+
+
+tek_register("Interface\\Icons\\INV_Hammer_01", function(self)
+	local hit, crit, miss, dodge, glance, parry, block = gettable(true, false, true)
+	local dhit, dcrit, dmiss, ddodge, dglance, dparry, dblock = gettable(true, true, true)
+	local shit, scrit, smiss, sdodge, sglance, sparry, sblock = gettable(false, false, true)
+
+
+	tip:SetPoint("BOTTOMLEFT", self, "TOPRIGHT")
+
+	tip:AddLine("Tank Melee Tables", 1,1,1)
+	tip:AddMultiLine("", "Normal", "Dual", "Special")
+
+	tip:AddMultiLine(L["Critical"], string.format("%.2f%%", crit),   string.format("%.2f%%", dcrit),   string.format("%.2f%%", scrit),   nil,nil,nil, .5,1,.5, .5,1,.5, .5,1,.5)
+	tip:AddMultiLine(L["Hit"],      string.format("%.2f%%", hit),    string.format("%.2f%%", dhit),    string.format("%.2f%%", shit),    nil,nil,nil, 1,1,1, 1,1,1, 1,1,1)
+	tip:AddMultiLine(L["Miss"],     string.format("%.2f%%", miss),   string.format("%.2f%%", dmiss),   string.format("%.2f%%", smiss),   nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
+	tip:AddMultiLine(L["Dodge"],    string.format("%.2f%%", dodge),  string.format("%.2f%%", ddodge),  string.format("%.2f%%", sdodge),  nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
+	tip:AddMultiLine(L["Parry"],    string.format("%.2f%%", parry),  string.format("%.2f%%", dparry),  string.format("%.2f%%", sparry),  nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
+	tip:AddMultiLine(L["Block"],    string.format("%.2f%%", block),  string.format("%.2f%%", dblock),  string.format("%.2f%%", sblock),  nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
+	tip:AddMultiLine(L["Glancing"], string.format("%.2f%%", glance), string.format("%.2f%%", dglance), string.format("%.2f%%", sglance), nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
+
+	tip:AddLine(" ")
+	tip:AddLine("All values are vs. mobs 3 levels above the player, attacking from in front, with capped weapon skill.", 0,1,0, true)
+
+	tip:Show()
+end, tip)
