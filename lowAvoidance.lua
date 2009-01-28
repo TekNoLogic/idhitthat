f3f70cbb88a0e836aabe97fd9aebf79ae009aef9
Tekkub <tekkub@gmail.com>
2009-01-27 21:05:13 -0700

    Generalize addon core, add spell table

--- lowAvoidance.lua
+++ lowAvoidance.lua
@@ -1,117 +1,46 @@
+
 local L = setmetatable({}, {__index=function(t,i) return i end})
+tek_locale = L
 
-local result = {
-	miss     = 0,
-	dodge    = 0,
-	parry    = 0,
-	block    = 0,
-	critical = 0,
-	hit      = 0,
-}
-
-local talentedcritreduct = 0
+---------------------------
+--      Basic Panel      --
+---------------------------
 
 local f = CreateFrame("Button", nil, CharacterModelFrame)
 f:SetFrameStrata("DIALOG")
 f:SetPoint("BOTTOMRIGHT", -4, 25)
 f:SetWidth(26) f:SetHeight(26)
 
-local icon = f:CreateTexture(nil, "BACKGROUND")
-icon:SetAllPoints()
-icon:SetTexture("Interface\\Icons\\Ability_Warrior_DefensiveStance")
-
-f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
-
-local nounit = {COMBAT_RATING_UPDATE = true, CHARACTER_POINTS_CHANGED = true, PLAYER_LOGIN = true}
-local function UpdateHitTable(self, event, unit)
-	if not nounit[event] and unit ~= "player" then return end
-
-	local defskillmod = GetDodgeBlockParryChanceFromDefense() - 0.6
-
-	result.miss = math.max(5 + defskillmod, 0)
-	result.dodge = math.max(0, GetDodgeChance() - 0.6)
-	result.parry = math.max(0, GetParryChance() - 0.6)
-	result.block = math.max(0, GetBlockChance() - 0.6)
-	result.critical = math.max(5 - defskillmod - talentedcritreduct - GetCombatRatingBonus(CR_CRIT_TAKEN_MELEE), 0)
-	result.hit = 0
-
-	local mainhand = GetInventoryItemLink("player", 16)
-	if not mainhand then result.parry = 0 else
-		local _, _, _, _, _, _, _, _, mainhandtype = GetItemInfo(mainhand)
-		if (mainhandtype ~= "INVTYPE_WEAPON") and (mainhandtype ~= "INVTYPE_WEAPONMAINHAND") and (mainhandtype ~= "INVTYPE_2HWEAPON") then
-			result.parry = 0
-		end
-	end
-
-	local offhand = GetInventoryItemLink("player", 17)
-	if not offhand then result.block = 0 else
-		local _, _, _, _, _, _, _, _, offhandtype = GetItemInfo(offhand)
-		if offhandtype ~= "INVTYPE_SHIELD" then result.block = 0 end
-	end
-
-	local leftover = 100
-
-	result.miss = min(result.miss, leftover)
-	leftover = leftover - result.miss
-
-	result.dodge = min(result.dodge, leftover)
-	leftover = leftover - result.dodge
-
-	result.parry = min(result.parry, leftover)
-	leftover = leftover - result.parry
-
-	result.block = min(result.block, leftover)
-	leftover = leftover - result.block
-
-	result.critical = min(result.critical, leftover)
-	leftover = leftover - result.critical
-
-	result.hit = leftover
-end
-
-function f:PLAYER_LOGIN()
-	for _,e in pairs{"UNIT_DAMAGE","PLAYER_DAMAGE_DONE_MODS", "UNIT_ATTACK_SPEED", "UNIT_RANGEDDAMAGE", "UNIT_ATTACK", "UNIT_STATS", "UNIT_RANGED_ATTACK_POWER", "COMBAT_RATING_UPDATE"} do
-		self:RegisterEvent(e)
-		self[e] = UpdateHitTable
-	end
-
-	self:UnregisterEvent("PLAYER_LOGIN")
-	self.PLAYER_LOGIN = nil
-
-	local _, class = UnitClass("player")
-
-	-- SoTF
-	if ( class == "DRUID" ) then
-		function self:CHARACTER_POINTS_CHANGED() talentedcritreduct = select(5, GetTalentInfo(2, 16)) end
-		self:RegisterEvent("CHARACTER_POINTS_CHANGED")
-		self:CHARACTER_POINTS_CHANGED()
-	-- SoH
-	elseif ( class == "ROGUE" ) then
-		function self:CHARACTER_POINTS_CHANGED() talentedcritreduct = select(5, GetTalentInfo(3, 3)) end
-		self:RegisterEvent("CHARACTER_POINTS_CHANGED")
-		self:CHARACTER_POINTS_CHANGED()
-	end
-
-	UpdateHitTable(nil, "PLAYER_LOGIN")
+local icons, onenters = {}, {}
+function tek_register(icon, onenter)
+	table.insert(onenters, onenter)
+	icons[onenter] = icon
 end
 
 
-f:SetScript("OnLeave", function() GameTooltip:Hide() end)
-f:SetScript("OnEnter", function(self)
- 	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-	GameTooltip:ClearLines()
+local tableindex, func
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, addon)
+	if addon:lower() ~= "lowavoidance" then return end
 
-	GameTooltip:AddLine("Attack Table")
+	tableindex, func = 1, onenters[1]
 
-	GameTooltip:AddDoubleLine(L["Miss"], string.format("%.2f%%", result.miss), nil,nil,nil, 1,1,1)
-	GameTooltip:AddDoubleLine(L["Dodge"], string.format("%.2f%%", result.dodge), nil,nil,nil, 1,1,1)
-	GameTooltip:AddDoubleLine(L["Parry"], string.format("%.2f%%", result.parry), nil,nil,nil, 1,1,1)
-	GameTooltip:AddDoubleLine(L["Block"], string.format("%.2f%%", result.block), nil,nil,nil, 1,1,1)
-	GameTooltip:AddDoubleLine(L["Critical"], string.format("%.2f%%", result.critical), nil,nil,nil, 1,0,0)
-	GameTooltip:AddDoubleLine(L["Hit"], string.format("%.2f%%", result.hit), nil,nil,nil, 1,.5,0)
+	f:SetNormalTexture(icons[func])
 
-	GameTooltip:Show()
+	self:UnregisterEvent("ADDON_LOADED")
+	f:SetScript("OnEvent", nil)
 end)
 
 
-if IsLoggedIn() then f:PLAYER_LOGIN() else f:RegisterEvent("PLAYER_LOGIN") end
+local function changetable(self, ...)
+	tableindex = tableindex + 1;
+	if tableindex > #onenters then tableindex = 1 end
+	func = onenters[tableindex]
+	f:SetNormalTexture(icons[func])
+	func(self)
+ end
+f:SetScript("OnClick", changetable)
+f:SetScript("OnMouseWheel", changetable)
+f:SetScript("OnLeave", function() GameTooltip:Hide() end)
+f:SetScript("OnEnter", function(self, ...) func(self, ...) end)
+f:EnableMouseWheel(true)
---
+++ spell.lua
@@ -0,0 +1,65 @@
+
+local L = tek_locale
+
+
+local _, class = UnitClass("player")
+
+
+tek_register("Interface\\Icons\\Spell_Holy_Aspiration", function(self)
+	local hit = math.min(100, 83 + GetCombatRatingBonus(CR_HIT_SPELL))
+	local crit = GetSpellCritChance(1)
+
+	local hasHP = UnitAura("player", "Heroic Presence")
+	if hasHP then hit = hit + 1 end
+
+	local _, sf, sfv, mis, misv, bop, bopv, iff, iffv
+
+	if class == "PRIEST" then
+		sf, _, _, _, sfv = GetTalentInfo(3,6)
+		mis, _, _, _, misv = GetTalentInfo(3,22)
+		if sfv > 0 then hit, sfv = hit + sfv, "+"..sfv.."%" else sfv = nil end
+		if misv > 0 then hit, misv = hit + misv, "+"..misv.."%" else misv = nil end
+	end
+
+	if class == "DRUID" then
+		bop, _, _, _, bopv = GetTalentInfo(1,17)
+		iff, _, _, _, iffv = GetTalentInfo(1,20)
+		if bopv > 0 then hit, bopv = hit + bopv*2, "+"..(bopv*2).."%" else bopv = nil end
+		if iffv > 0 then hit, iffv = hit + iffv, "+"..(iffv).."%**" else iffv = nil end
+	end
+
+	local miss = 100 - hit
+	crit = crit * hit/100
+	hit = hit - crit
+
+
+ 	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:ClearLines()
+
+	GameTooltip:AddLine("Spell Table", 1,1,1)
+
+	GameTooltip:AddDoubleLine(L["Critical"], string.format("%.2f%%", crit), nil,nil,nil, .5,1,.5)
+	GameTooltip:AddDoubleLine(L["Hit"], string.format("%.2f%%", hit), nil,nil,nil, 1,1,1)
+	GameTooltip:AddDoubleLine(L["Miss"], string.format("%.2f%%", miss), nil,nil,nil, 1,0,0)
+
+	if hasHP or sfv or misv or sfv or bopv or iffv then
+		GameTooltip:AddLine(" ")
+		GameTooltip:AddLine("Hit Bonuses", 1,1,1)
+		if hasHP then GameTooltip:AddDoubleLine(hasHP, "+1%", nil,nil,nil, 1,1,1) end
+		if sfv   then GameTooltip:AddDoubleLine(sf.."*", sfv, nil,nil,nil, 1,1,1) end
+		if misv  then GameTooltip:AddDoubleLine(mis.."**", misv, nil,nil,nil, 1,1,1) end
+		if bopv  then GameTooltip:AddDoubleLine(bop, bopv, nil,nil,nil, 1,1,1) end
+		if iffv  then GameTooltip:AddDoubleLine(iff.."**", iffv, nil,nil,nil, 1,1,1) end
+
+		if sfv then GameTooltip:AddLine("*Shadow spells only", 0.5, 0.5, 1) end
+		if misv or iffv then GameTooltip:AddLine("**When debuff is applied", 0.5, 0.5, 1) end
+	end
+
+	GameTooltip:AddLine(" ")
+	GameTooltip:AddLine("All values are vs. mobs\n3 levels above the player.", 0,1,0, true)
+
+	GameTooltip:Show()
+end)
+
+
+tek_locale, tek_register = nil
