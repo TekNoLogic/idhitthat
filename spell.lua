9385b77ca703d3058c71cf1e5f5ae2dfb28f89bc
Tekkub <tekkub@gmail.com>
2009-01-28 23:15:10 -0700

    Use more tha astrisks in spell table notes

--- spell.lua
+++ spell.lua
@@ -25,7 +25,7 @@ tek_register("Interface\\Icons\\Spell_Holy_Aspiration", function(self)
 		bop, _, _, _, bopv = GetTalentInfo(1,17)
 		iff, _, _, _, iffv = GetTalentInfo(1,20)
 		if bopv > 0 then hit, bopv = hit + bopv*2, "+"..(bopv*2).."%" else bopv = nil end
-		if iffv > 0 then hit, iffv = hit + iffv, "+"..(iffv).."%**" else iffv = nil end
+		if iffv > 0 then hit, iffv = hit + iffv, "+"..(iffv).."%" else iffv = nil end
 	end
 
 	local miss = 100 - hit
@@ -46,13 +46,16 @@ tek_register("Interface\\Icons\\Spell_Holy_Aspiration", function(self)
 		GameTooltip:AddLine(" ")
 		GameTooltip:AddLine("Hit Bonuses", 1,1,1)
 		if hasHP then GameTooltip:AddDoubleLine(hasHP, "+1%", nil,nil,nil, 1,1,1) end
-		if sfv   then GameTooltip:AddDoubleLine(sf.."*", sfv, nil,nil,nil, 1,1,1) end
-		if misv  then GameTooltip:AddDoubleLine(mis.."**", misv, nil,nil,nil, 1,1,1) end
+		if misv  then GameTooltip:AddDoubleLine(mis.."*", misv, nil,nil,nil, 1,1,1) end
+		if sfv   then GameTooltip:AddDoubleLine(sf.."\226\128\160", sfv, nil,nil,nil, 1,1,1) end
 		if bopv  then GameTooltip:AddDoubleLine(bop, bopv, nil,nil,nil, 1,1,1) end
-		if iffv  then GameTooltip:AddDoubleLine(iff.."**", iffv, nil,nil,nil, 1,1,1) end
+		if iffv  then GameTooltip:AddDoubleLine(iff.."*", iffv, nil,nil,nil, 1,1,1) end
+--~ 		† ‡ º ¹ ² ³ •
+--~ 		â€ â€¡â€¢ÂºÂ¹Â²Â³
+--~ 		\226\128\160 \226\128\161 \226\128\162 \194\186 \194\185 \194\178 \194\179
 
-		if sfv then GameTooltip:AddLine("*Shadow spells only", 0.5, 0.5, 1) end
-		if misv or iffv then GameTooltip:AddLine("**When debuff is applied", 0.5, 0.5, 1) end
+		if misv or iffv then GameTooltip:AddLine("*When debuff is applied", 0.5, 0.5, 1) end
+		if sfv then GameTooltip:AddLine("\226\128\160Shadow spells only", 0.5, 0.5, 1) end
 	end
 
 	GameTooltip:AddLine(" ")
