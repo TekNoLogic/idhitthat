2d71cb2eab4a0d7c854a4c872238daa523805ad5
Tekkub <tekkub@gmail.com>
2009-03-26 18:02:52 -0600

    Add Heroic Presence and hunter hit talent to ranged table

--- ranged.lua
+++ ranged.lua
@@ -12,7 +12,17 @@ local function gettable(infront)
 
 	local leftover = 100
 
-	miss = math.min(miss, leftover)
+	local hasHP = UnitAura("player", "Heroic Presence")
+	if hasHP then miss = miss - 1 end
+
+	local _, fa, fav
+
+	if class == "HUNTER" then
+		fa, _, _, _, fav = GetTalentInfo(2,2)
+		if fav > 0 then miss = miss - fav else fav = nil end
+	end
+
+	miss = math.max(0, math.min(miss, leftover))
 	leftover = leftover - miss
 
 	block = math.min(block, leftover)
@@ -23,12 +33,12 @@ local function gettable(infront)
 
 	hit = leftover
 
-	return hit, crit, miss, block
+	return hit, crit, miss, block, hasHP, fa, fav
 end
 
 
 tek_register("Interface\\Icons\\INV_Misc_Ammo_Arrow_05", function(self)
-	local hit, crit, miss, block = gettable()
+	local hit, crit, miss, block, hasHP, fa, fav = gettable()
 	local fhit, fcrit, fmiss, fblock = gettable(true)
 
 
@@ -41,6 +51,13 @@ tek_register("Interface\\Icons\\INV_Misc_Ammo_Arrow_05", function(self)
 	tip:AddMultiLine(L["Hit"],      string.format("%.2f%%", hit),    string.format("%.2f%%", fhit),    nil,nil,nil, 1,1,1, 1,1,1)
 	tip:AddMultiLine(L["Miss"],     string.format("%.2f%%", miss),   string.format("%.2f%%", fmiss),   nil,nil,nil, 1,.5,0, 1,.5,0)
 	tip:AddMultiLine(L["Block"],    string.format("%.2f%%", block),  string.format("%.2f%%", fblock),  nil,nil,nil, 1,.5,0, 1,.5,0)
+
+	if hasHP or fav then
+		tip:AddLine(" ")
+		tip:AddLine("Hit Bonuses", 1,1,1)
+		if hasHP then tip:AddLine(hasHP.. " |cffffffff(+1%)") end
+		if fav then tip:AddLine(fa.. " |cffffffff(+"..fav.."%)") end
+	end
 
 	tip:AddLine(" ")
 	tip:AddLine("All values are vs. mobs 3 levels above the player, with capped weapon skill.", 0,1,0, true)
