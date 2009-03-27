c134e6a51520f4cb58d7146bc48a70cde19e383c
Tekkub <tekkub@gmail.com>
2009-03-26 23:34:56 -0600

    Remove dual-wield column from the tank attack tables, tanks should never be dual-wielding

--- melee.lua
+++ melee.lua
@@ -80,33 +80,33 @@ tek_register("Interface\\Icons\\INV_Sword_26", function(self)
 end, tip)
 
 
+local tip2 = LibStub("tektip-1.0").new(3, "LEFT", "RIGHT", "RIGHT")
 tek_register("Interface\\Icons\\INV_Hammer_01", function(self)
 	local hit, crit, miss, dodge, glance, parry, block, hasHP = gettable(true, false, true)
-	local dhit, dcrit, dmiss, ddodge, dglance, dparry, dblock = gettable(true, true, true)
 	local shit, scrit, smiss, sdodge, sglance, sparry, sblock = gettable(false, false, true)
 
 
-	tip:SetPoint("BOTTOMLEFT", self, "TOPRIGHT")
+	tip2:SetPoint("BOTTOMLEFT", self, "TOPRIGHT")
 
-	tip:AddLine("Tank Melee Tables", 1,1,1)
-	tip:AddMultiLine("", "Normal", "Dual", "Special")
+	tip2:AddLine("Tank Melee Tables", 1,1,1)
+	tip2:AddMultiLine("", "Normal", "Special")
 
-	tip:AddMultiLine(L["Critical"], string.format("%.2f%%", crit),   string.format("%.2f%%", dcrit),   string.format("%.2f%%", scrit),   nil,nil,nil, .5,1,.5, .5,1,.5, .5,1,.5)
-	tip:AddMultiLine(L["Hit"],      string.format("%.2f%%", hit),    string.format("%.2f%%", dhit),    string.format("%.2f%%", shit),    nil,nil,nil, 1,1,1, 1,1,1, 1,1,1)
-	tip:AddMultiLine(L["Miss"],     string.format("%.2f%%", miss),   string.format("%.2f%%", dmiss),   string.format("%.2f%%", smiss),   nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
-	tip:AddMultiLine(L["Dodge"],    string.format("%.2f%%", dodge),  string.format("%.2f%%", ddodge),  string.format("%.2f%%", sdodge),  nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
-	tip:AddMultiLine(L["Parry"],    string.format("%.2f%%", parry),  string.format("%.2f%%", dparry),  string.format("%.2f%%", sparry),  nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
-	tip:AddMultiLine(L["Block"],    string.format("%.2f%%", block),  string.format("%.2f%%", dblock),  string.format("%.2f%%", sblock),  nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
-	tip:AddMultiLine(L["Glancing"], string.format("%.2f%%", glance), string.format("%.2f%%", dglance), string.format("%.2f%%", sglance), nil,nil,nil, 1,.5,0, 1,.5,0, 1,.5,0)
+	tip2:AddMultiLine(L["Critical"], string.format("%.2f%%", crit),   string.format("%.2f%%", scrit),   nil,nil,nil, .5,1,.5, .5,1,.5)
+	tip2:AddMultiLine(L["Hit"],      string.format("%.2f%%", hit),    string.format("%.2f%%", shit),    nil,nil,nil, 1,1,1, 1,1,1)
+	tip2:AddMultiLine(L["Miss"],     string.format("%.2f%%", miss),   string.format("%.2f%%", smiss),   nil,nil,nil, 1,.5,0, 1,.5,0)
+	tip2:AddMultiLine(L["Dodge"],    string.format("%.2f%%", dodge),  string.format("%.2f%%", sdodge),  nil,nil,nil, 1,.5,0, 1,.5,0)
+	tip2:AddMultiLine(L["Parry"],    string.format("%.2f%%", parry),  string.format("%.2f%%", sparry),  nil,nil,nil, 1,.5,0, 1,.5,0)
+	tip2:AddMultiLine(L["Block"],    string.format("%.2f%%", block),  string.format("%.2f%%", sblock),  nil,nil,nil, 1,.5,0, 1,.5,0)
+	tip2:AddMultiLine(L["Glancing"], string.format("%.2f%%", glance), string.format("%.2f%%", sglance), nil,nil,nil, 1,.5,0, 1,.5,0)
 
 	if hasHP then
-		tip:AddLine(" ")
-		tip:AddLine("Hit Bonuses", 1,1,1)
-		if hasHP then tip:AddLine(hasHP.. " |cffffffff(+1%)") end
+		tip2:AddLine(" ")
+		tip2:AddLine("Hit Bonuses", 1,1,1)
+		if hasHP then tip2:AddLine(hasHP.. " |cffffffff(+1%)") end
 	end
 
-	tip:AddLine(" ")
-	tip:AddLine("All values are vs. mobs 3 levels above the player, attacking |cffccffccfrom in front|r, with capped weapon skill.", 0,1,0, true)
+	tip2:AddLine(" ")
+	tip2:AddLine("All values are vs. mobs 3 levels above the player, attacking |cffccffccfrom in front|r, with capped weapon skill.", 0,1,0, true)
 
-	tip:Show()
-end, tip)
+	tip2:Show()
+end, tip2)
